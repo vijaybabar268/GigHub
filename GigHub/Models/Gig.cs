c784c533dcaff99058d381ea2c77917159814a9c
vijay babar <vijbab.kalp@gmail.com>
2020-06-28 23:52:28 +0530

    Implement Saving a Gig.

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -6,17 +6,21 @@ namespace GigHub.Models
     public class Gig
     {
         public int Id { get; set; }
+                
+        public ApplicationUser Artist { get; set; }
 
         [Required]
-        public ApplicationUser Artist { get; set; }
-        
+        public string ArtistId { get; set; }    
+
         public DateTime DateTime { get; set; }
 
         [Required]
         [StringLength(255)]
         public string Venue { get; set; }
+                
+        public Genre Genre { get; set; }
 
         [Required]
-        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }   
     }
 }
--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -6,9 +6,13 @@ namespace GigHub.ViewModels
     public class GigFormViewModel
     {
         public string Venue { get; set; }
+        
         public string Date { get; set; }
+        
         public string Time { get; set; }
-        public int Genre { get; set; }
+        
+        public byte Genre { get; set; }
+        
         public IEnumerable<Genre> Genres { get; set; }
     }
 }
